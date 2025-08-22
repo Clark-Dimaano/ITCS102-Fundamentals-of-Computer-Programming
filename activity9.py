46dba18707dc8c2d19615bb22492d454e9f01b6d
Clark-Dimaano <dimaanoclark0@gmail.com>
2025-08-22 23:34:48 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,14 @@
+a = 3
+b = 2
+
+c = 6
+d = 8
+
+username = 'clark'
+password = 'dimaano123'
+
+#print("USER LOGIN")
+#print( (username == 'clark') and (password == 'dimaano123') )
+
+print( (a < b) and (c > d) )
+print( (a < b) or (c > d) )
