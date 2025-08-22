0739e6ab8585b792c0c174530d4d706abcd01af2
Clark-Dimaano <dimaanoclark0@gmail.com>
2025-08-22 23:09:32 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,22 @@
+a = 3
+
+print("The value of a is",a)
+
+a += 7
+
+print("The value of a is",a)
+
+a += 5
+print("The value of a is",a)
+
+a += 15
+print("The value of a is",a)
+
+a -= 2
+print("The value of a is",a)
+
+a *= 5
+print("The value of a is",a)
+
+a = a + 8
+print("The value of a is",a)
