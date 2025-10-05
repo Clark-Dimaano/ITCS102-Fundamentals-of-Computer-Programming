8ffbaf67633918ae9aa7c184d91381fbc0ed90a7
Clark-Dimaano <dimaanoclark0@gmail.com>
2025-10-05 15:15:35 +0800

    Update code_challenge12.py

--- code_challenge12.py
+++ code_challenge12.py
@@ -1,9 +1,8 @@
 for i in range(1,7,1):
 	for y in range(7,i,-1):
 		print(" ", end = " ")
-	for x in range(1,i,1):
-		print(i, end = " ")
-	for z in range(1,i,1):
-		print(i, end = " ")
-
-	print(i)
+	for x in range(i,1,-1):
+		print(x, end = " ")
+	for z in range(1,i + 1):
+		print(z, end =" ")
+	print()
