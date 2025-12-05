f6a6ec890486615dfbdaefb4f0cff35c3e212424
Clark-Dimaano <dimaanoclark0@gmail.com>
2025-12-05 15:19:00 +0800

    Update Finalsproject.py

--- Finalsproject.py
+++ Finalsproject.py
@@ -6,7 +6,7 @@ print(f"Hi {name}, welcome to code compiler")
 
 yn = input("\nWould you like to use my code compiler? (yes/no): ")
 while True:  
-    os.system('cls')
+    
     if yn == 'yes':
         print("=================================================")
         print("A - Functions ")
@@ -17,6 +17,7 @@ while True:
         
         
         select = input("Select what command should we run: ").lower()
+        
         if select == 'a':
             print("1 - Built-in Function") 
             print("2 - User-Defined Function ")
@@ -24,7 +25,13 @@ while True:
             if x == '1':
                 print("=====================================================================================")
                 print("Here are some of the function and its use:")
-                print("\nprint() -    Displays text, numbers, or variables on the screen.\n           It’s the most commonly used function for showing program output.")
+                print("\nprint() -    Displays text, numbers, or variables on the screen.\n             It’s the most commonly used function for showing program output.")
+                print("               Example: ")
+                print("               name = 'Alice'")
+                print("               age = 25")
+                print("               print('Name:', name, 'Age:', age)")
+                print("\n               Output: ")
+                print("               Name: Alice, Age: 25 ")
             elif x == '2':
                 pass
             else:
