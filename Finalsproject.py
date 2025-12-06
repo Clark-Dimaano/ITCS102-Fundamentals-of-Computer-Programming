e39b35061e87d56679cd0a6520b31ac403cec222
Clark-Dimaano <dimaanoclark0@gmail.com>
2025-12-06 11:59:00 +0800

    Update Finalsproject.py

--- Finalsproject.py
+++ Finalsproject.py
@@ -1,12 +1,12 @@
+
 import os
 
 name = input("Hello user, please input your name: ").title()
 print(f"Hi {name}, welcome to code compiler")
 
-
-yn = input("\nWould you like to use my code compiler? (yes/no): ")
+yn = input("\nWould you like to use my code compiler? (yes/no): ") 
+os.system('cls')
 while True:  
-    
     if yn == 'yes':
         print("=================================================")
         print("A - Functions ")
@@ -14,20 +14,28 @@ while True:
         print("C - If/Elif Else")
         print("D - List and Dictionary")
         print("X - Exit Menu")
+        print("=================================================")
+
         
         
         select = input("Select what command should we run: ").lower()
         
         if select == 'a':
+            os.system('cls')
             print("============================================================================")
             print("1 - Built-in Function") 
             print("2 - User-Defined Function ")
+            print("============================================================================")
             x = input("Input the function should we use: ")
 
             if x == '1':
+                os.system('cls')
                 print("===================================================================")
                 print("1 - Definitions\n2 - Run the code")
+                print("===================================================================")
                 y = input("Input your choice: ")
+
+                os.system('cls')
                 if y == '1':
                     print("=====================================================================================")
                     print("Here are some of the function and its use:")
@@ -45,21 +53,49 @@ while True:
                     print("\n               Output (if the user enters 20):")
                     print("               Enter your age: 20 ")
                     print("               You are 20 years old")
-                    print("\nRange() -    This function generates a sequence of numbers. It is commonly used in for loops. \n\t     You can specify a start, stop, and step value. It does not create a list in memory, which makes it memory-efficient.\n\t       Example 1 (simple loop):\n\t       for i in range(1,6,1):\n\t           print(i)\
+                    print("\nrange() -    This function generates a sequence of numbers. It is commonly used in for loops. \n\t     You can specify a start, stop, and step value. It does not create a list in memory, which makes it memory-efficient.\n\t       Example:\n\t       for i in range(1,6,1):\n\t           print(i)\
                         \n\n\t       Output:\n\t       1\n\t       2\n\t       3\n\t       4\n\t       5")
-                elif y == 2:
-                    pass
+                    print("\neval() -    This function takes a string argument containing a valid expression, evaluates it, and returns the result of that expression. \n\t    It can evaluate numbers, arithmetic expressions, lists, tuples, function calls, and more.\n\t       Example:\n\t       x = 3\n\t       y = 4\n\t       print(eval(x * y))\n\n\t     Output:\n\t       12")
+                    print("=====================================================================================")
+                    input("Press any key to go back: ")
+                    os.system('cls')
+                elif y == '2':
+                    print("=====================================================================================")
+
+                    name = 'Alice'
+                    age = 20
+                    print("Name:", name, ",Age:", age)
+
+                    age = input("\nEnter your age: ")
+                    print(f"Your age is {age} years old\n")
+                    print("\n====================================\n(for i in range(1,6,1):)\n\t(print(i))")
+                    for i in range(1,6,1):
+                        print(i)
+                    print("====================================")
+                
+                    print("\nx * y = \nInput a number to multiply:")              
+                    x = eval(input("x = "))
+                    y = eval(input("y = "))
+                    ans = x * y 
+                    print(f"\nThe answer from what you input is {ans}")
+                    print("=====================================================================================")
+                    input("Press any key to go back: ")      
+                    
             else:
+                os.system('cls')
                 print("Select again")
-                continue
+                
+                
         elif select == 'x':
             print("You have exited the program")
             break
-             
+        else:
+            print("Invalid Choice, try again")     
+            continue   
 
     elif yn == 'no':
         print("Thanks for using it")
         break
     else:
-        print("Invalid Choice, try again")
-        continue
+        print("\nInvalid Choice, try again")
+        
